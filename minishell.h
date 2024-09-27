1c5ad97cffc24e98ff4ac4fd6b34d7ead4f3c39c
noah <147538606+gordobil@users.noreply.github.com>
2024-09-27 20:32:42 +0200

    minishell.h

--- minishell.h
+++ minishell.h
@@ -1,7 +1,16 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+
+typedef struct s_info
+{
+	char	*args;
+}							t_info
+
+#endif
