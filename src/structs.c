572e2a7f92b66b2681e5b209e605ef4567e3c432
gordobil <noahgordobil@gmail.com>
2024-10-29 19:44:03 +0100

    ya no peta

--- src/structs.c
+++ src/structs.c
@@ -34,7 +34,8 @@ void	dup_args(char **arg_matrix, t_mini *mini, int i)
 
 	j = 0;
 	k = 0;
-	while ((ft_strcmp(arg_matrix[i], "|") != 0) && (arg_matrix[i] != NULL))
+	while ((ft_strcmp(arg_matrix[i], "|") != 0) && (arg_matrix[i] != NULL)
+			&& (mini->pipes->command[j] != NULL))
 	{
 		if (arg_matrix[i][0] != '<' && arg_matrix[i][0] != '>')
 		{
@@ -50,10 +51,11 @@ void	dup_args(char **arg_matrix, t_mini *mini, int i)
 			}
 			i++;
 		}
-		if (arg_matrix[i] == NULL)
+		if (arg_matrix[i] == NULL || ft_strcmp(arg_matrix[i], "|") == 0)
 			break ;
 	}
 	mini->pipes->command[j] = NULL;
+	mini->files[k] = NULL;
 }
 
 int	count_args(char **arg_matrix, t_mini *mini, int i, char ret)
@@ -61,7 +63,7 @@ int	count_args(char **arg_matrix, t_mini *mini, int i, char ret)
 	int	count;
 
 	count = 0;
-	while (arg_matrix[++i] != NULL)
+	while (arg_matrix[i] != NULL)
 	{
 		if (ft_strcmp(arg_matrix[i], "|") == 0)
 			break ;
@@ -77,6 +79,7 @@ int	count_args(char **arg_matrix, t_mini *mini, int i, char ret)
 			mini->file_c++;
 			mini->outfile = ft_strdup(arg_matrix[i]);
 		}
+		i++;
 	}
 	if (ret == 'i')
 		return (i);
@@ -88,6 +91,7 @@ void	pipe_info(char **arg_matrix, t_mini *mini)
 	t_pipes	*prev_node;
 	int		i;
 	int		position;
+	int		arguments;
 
 	mini->arg_matrix = NULL;
 	mini->infile = NULL;
@@ -101,13 +105,16 @@ void	pipe_info(char **arg_matrix, t_mini *mini)
 		mini->pipes = malloc(sizeof(t_pipes));
 		mini->pipes->mini = mini;
 		mini->pipes->position = position;
-		mini->pipes->command = malloc((count_args(arg_matrix, mini, i - 1, 'c') + 1)
-				* sizeof(char *));
+		arguments = count_args(arg_matrix, mini, i, 'c');
+		mini->pipes->command = malloc((arguments + 1) * sizeof(char *));
+		mini->pipes->command[arguments] = NULL;
 		mini->files = malloc(mini->file_c * sizeof(char *));
-		mini->files[mini->file_c - 1] = NULL;
-		if (count_args(arg_matrix, mini, i - 1, 'c') == mini->arg_c)
+		ft_printf("1\n");
+		if (arguments == mini->arg_c)
 			return (single_command(arg_matrix, mini));
+		ft_printf("2\n");
 		dup_args(arg_matrix, mini, i);
+		ft_printf("3\n");
 		if (position == 0)
 			mini->pipes->prev = NULL;
 		else
@@ -117,14 +124,15 @@ void	pipe_info(char **arg_matrix, t_mini *mini)
 		}
 		prev_node = mini->pipes;
 		ft_printf("%m\n", mini->pipes->command);
-		if (arg_matrix[count_args(arg_matrix, mini, i - 1, 'i')] == NULL)
+		if (arg_matrix[count_args(arg_matrix, mini, i, 'i')] == NULL)
 		{
 			mini->pipes->next = NULL;
 			break ;
 		}
-		i = count_args(arg_matrix, mini, i - 1, 'i');
+		i = count_args(arg_matrix, mini, i, 'i');
 		if (ft_strcmp(arg_matrix[i], "|") == 0)
 			i++;
+		ft_printf("4\n");
 		mini->pipes = mini->pipes->next;
 		position++;
 	}
