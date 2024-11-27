1823b1a70c6ed5f86f597a9e52bf128538833b4c
Noah Gordobil Merino <ngordobi@c3r7s1.42urduliz.com>
2024-11-27 19:51:12 +0100

    no such file or directory error

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: ngordobi <ngordobi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:06:42 by ngordobi          #+#    #+#             */
-/*   Updated: 2024/11/27 15:16:58 by ngordobi         ###   ########.fr       */
+/*   Updated: 2024/11/27 19:50:29 by ngordobi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	error_messages(int error, char *str)
 	else if (error == -2)
 		ft_printf("error: unclosed double quotes\n");
 	else if (error == -3)
-		ft_printf("minishell: %s: No such file or directory\n", str);
+		ft_printf("%s: No such file or directory\n", str);
 	else if (error == -4)
-		ft_printf("minishell: %s: Error\n", str);
+		ft_printf("%s: Error\n", str);
 }
